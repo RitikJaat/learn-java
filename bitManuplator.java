a73c8c5c87605df3a4eefc189da88ab75d744c7e
M0NSTER <ritikjat990@gmail.com>
2023-07-24 22:43:25 +0530

    case 2 added

--- bitManuplator.java
+++ bitManuplator.java
@@ -24,6 +24,22 @@ public class bitManuplator {
                 break;
 
             case 2:
+                System.out.println("Enter a value for bit");
+                int n1 = sc.nextInt();
+                System.out.println("Enter the postion you would like to get");
+                int p1 = sc.nextInt();
+                int bitMask1 = 1 << p1;
+                /*example
+                 * n1 = 5 ; in bits-> 0101
+                 * p1 = 1 ; in bits-> 0111
+                 * used | to do OR
+                 *  0 1 0 1
+                 *  0 0 1 0
+                 * =0 1 1 1
+                 */
+
+                int newNumber = bitMask1 | n1;
+                System.out.println(newNumber);
                 break;
             default:
                 System.out.println("Enter a valid option.");
