98c9cc506936a387fa24abd63fed701c5c11f912
M0NSTER <ritikjat990@gmail.com>
2023-07-24 22:58:24 +0530

    case 3 updated

--- bitManuplator.java
+++ bitManuplator.java
@@ -6,14 +6,15 @@ public class bitManuplator {
         System.out.println("Choose from the given options below : ");
         System.out.println("1) Get Bit : ");
         System.out.println("2) Set Bit : ");
+        System.out.println("3) Clear Bit : ");
         int option = sc.nextInt();
 
         switch (option) {
             case 1:
                 System.out.println("Enter a value for bit");
-                int n = sc.nextInt();
+                int n = sc.nextInt();// 5
                 System.out.println("Enter the postion you would like to get");
-                int p = sc.nextInt();
+                int p = sc.nextInt();// 2
                 int bitMask = 1 << p;
 
                 if ((bitMask & n) == 0) {
@@ -24,22 +25,39 @@ public class bitManuplator {
                 break;
 
             case 2:
+
                 System.out.println("Enter a value for bit");
-                int n1 = sc.nextInt();
-                System.out.println("Enter the postion you would like to get");
-                int p1 = sc.nextInt();
+                int n1 = sc.nextInt();// 5
+                System.out.println("Enter the postion you would like to set");
+                int p1 = sc.nextInt();// 2
                 int bitMask1 = 1 << p1;
-                /*example
+                /*
+                 * example
                  * n1 = 5 ; in bits-> 0101
                  * p1 = 1 ; in bits-> 0111
                  * used | to do OR
-                 *  0 1 0 1
-                 *  0 0 1 0
+                 * 0 1 0 1
+                 * 0 0 1 0
                  * =0 1 1 1
                  */
 
                 int newNumber = bitMask1 | n1;
                 System.out.println(newNumber);
+                break;
+            case 3:
+                System.out.println("Enter a value for bit");
+                int n2 = sc.nextInt(); // default number used 5
+                System.out.println("Enter the postion you would like to clear");
+                int p2 = sc.nextInt(); // default number used 2
+                int bitMask2 = 1 << p2;
+                int notBitMask = ~(bitMask2); // ~(bitmask) did made all bits opposite.
+                /*
+                 * example
+                 * bitmask 2 = 0100
+                 * ~bitmask -> 1011
+                 */
+                int newNumber1 = notBitMask & n2;
+                System.out.println(newNumber1);
                 break;
             default:
                 System.out.println("Enter a valid option.");
