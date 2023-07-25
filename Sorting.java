bdd87d8216f6ec576cda1a407174139cca5d80f0
M0NSTER <ritikjat990@gmail.com>
2023-07-25 11:49:53 +0530

    bubble sort

--- Sorting.java
+++ Sorting.java
@@ -1,22 +1,32 @@
 import java.util.*;
 
 public class Sorting {
+    public static void printArray(int array[]) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    };
+
     public static void main(String args[]) {
-        Scanner sc =  new Scanner(System.in);
-        int array[] = sc.nextInt(); 
+        Scanner sc = new Scanner(System.in);
+        int array[] = { 7, 8, 3, 1, 2 };
+
         // deafult used { 7, 8, 3, 1, 2 };
 
         // bubble sort
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = 0; j < array.length - i - 1; j++) {
-                if (array[i] > array[i + 1]) {
+                if (array[j] > array[j + 1]) {
                     // swap values
-                    int temp = array[i];
-                    array[i] = array[i + 1];
-                    array[i + 1] = temp;
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
                 }
             }
         }
+        printArray(array);
+        sc.close();
     }
 
 }
