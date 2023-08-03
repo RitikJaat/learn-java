08e84a5c4f18c58de7a7afa26dfa1c990760c0ef
M0NSTER <ritikjat990@gmail.com>
2023-08-03 15:42:07 +0530

    RecurrsionAdv complete

--- RecurrsionAdv.java
+++ RecurrsionAdv.java
@@ -17,7 +17,7 @@ public class RecurrsionAdv {
     };
 
     public static int countPath(int i, int j, int n, int m) {
-        if(i==n || j==m){
+        if (i == n || j == m) {
             return 0;
         }
         if (i == n - 1 && j == m - 1) {
@@ -32,17 +32,78 @@ public class RecurrsionAdv {
         return downPaths + rightPath;
     };
 
+    public static int printTiles(int n, int m) {
+
+        if (n == m) {
+            return 2;
+        }
+        if (n < m) {
+            return 1;
+        }
+        // vertically place
+        int vPlace = printTiles(n - m, m);
+
+        // herizontal placement
+        int hPLace = printTiles(n - 1, m);
+        return vPlace + hPLace;
+    };
+
+    public static int callGuest(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        // single
+        int way1 = callGuest(n - 1);
+
+        // pair
+        int way2 = callGuest(n - 1) * callGuest(n - 2);
+        return way1 + way2;
+    }; 
+
+    public static void printSubset(ArrayList<Integer> subset){
+        for(int i=0;i<subset.size();i++){
+            System.out.print(subset.get(i)+" ");
+        }
+        System.out.println();
+    }
+
+    public static void findSubsets(int n, ArrayList<Integer> subset){
+        if(n==0){
+            printSubset(subset);
+            return;
+        }
+        // if added
+        subset.add(n);
+        findSubsets(n-1, subset);
+
+        // if not added
+        subset.remove(subset.size()-1);
+        findSubsets(n-1, subset);
+    }
+
     public static void main(String[] args) {
         // Permutation
-
         // String str = "abc";
         // printPermutations(str, "");
 
         // count paths
+        // int n = 3, m = 3;
+        // int total = countPath(0, 0, n, m);
+        // System.out.println(total);
 
-        int n = 3, m = 3;
-        int total = countPath(0, 0, n, m);
-        System.out.println(total);
+        // tiles placement
+        // int n = 4, m = 2;
+        // System.out.println(printTiles(n, m));
+
+        // call guests in pairs or alone
+        // int n = 4;
+        // System.out.println(callGuest(n));
+
+        // Set of natural n numbers
+        int n = 3;
+        ArrayList<Integer> subset = new ArrayList<>();
+        findSubsets(n, subset);
+
     }
 
 }
