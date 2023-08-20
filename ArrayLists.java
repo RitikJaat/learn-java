ce8142ae30204f1328cc32117f77e59dec7dfe3f
M0NSTER <ritikjat990@gmail.com>
2023-08-20 20:21:43 +0530

    .

--- ArrayLists.java
+++ ArrayLists.java
@@ -4,8 +4,8 @@ import java.util.Collections;
 public class ArrayLists {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<Integer>();
-        ArrayList<String> list2 = new ArrayList<String>();
-        ArrayList<Boolean> list3 = new ArrayList<>();
+        // ArrayList<String> list2 = new ArrayList<String>(); //example
+        // ArrayList<Boolean> list3 = new ArrayList<>(); //example
 
 
         // adding elements in intergers arraylist
