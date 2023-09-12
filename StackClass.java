c9cb51f483b4ece9fa2f92e8dd8fe071d397f3ee
M0NSTER <ritikjat990@gmail.com>
2023-09-11 21:14:06 -0700

    reverse stack

--- StackClass.java
+++ StackClass.java
@@ -1,8 +1,8 @@
 import java.util.*;
 
 public class StackClass {
-    public static void pudhAtBottom(int data, Stack<Integer> s){
-        if(s.isEmpty()){
+    public static void pudhAtBottom(int data, Stack<Integer> s) {
+        if (s.isEmpty()) {
             s.push(data);
             return;
         }
@@ -10,14 +10,24 @@ public class StackClass {
         pudhAtBottom(data, s);
         s.push(top);
     }
-       
+
+    public static void reverse(Stack<Integer> s) {
+        if (s.isEmpty()) {
+            return;
+        }
+        int top = s.pop();
+        reverse(s);
+        pudhAtBottom(top, s);
+    }
+
     public static void main(String[] args) {
         Stack<Integer> s = new Stack<>();
         s.push(1);
         s.push(2);
         s.push(3);
-        pudhAtBottom(4, s);
-
+        // pudhAtBottom(4, s);
+        reverse(s);
+        
         while (!s.isEmpty()) {
             System.out.println(s.peek());
             s.pop();
