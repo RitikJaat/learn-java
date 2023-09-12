ca40ce5eb8e5d0633d328d1e0a89bf4f9edb7c4a
M0NSTER <ritikjat990@gmail.com>
2023-09-11 20:07:06 -0700

    normal stack use

--- StackClass.java
+++ StackClass.java
@@ -1,38 +1,9 @@
 import java.util.*;
 
 public class StackClass {
-        static class Stack {
-        static ArrayList<Integer> list = new ArrayList<>();
-
-        public static boolean isEmpty(){
-            return list.size()==0;
-            
-        }
-
-        // push
-        public static void push (int data){
-            list.add(data);
-        }
-        // pop
-        public static int pop(){
-            if(isEmpty()){
-                return -1;
-            }
-            int top = list.get(list.size()-1);
-            list.remove(list.size()-1);
-            return top;
-        }
-        // peek
-        public static int peek(){
-            if(isEmpty()){
-                return -1;
-            }
-            return list.get(list.size()-1);
-        }
-    }
-
+       
     public static void main(String[] args) {
-        Stack s = new Stack();
+        Stack<Integer> s = new Stack<>();
         s.push(1);
         s.push(2);
         s.push(3);
