00806556d9513c35d787c67c43034d7d8a3ec162
M0NSTER <ritikjat990@gmail.com>
2023-09-13 23:25:05 -0700

    QueueY basic

---
+++ BinaryTreess.java
@@ -0,0 +1,6 @@
+public class BinaryTreess {
+    
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ QueueY.java
@@ -0,0 +1,64 @@
+public class QueueY {
+    static class Queue {
+        static int array[];
+        static int size;
+        static int rear = -1;
+
+        Queue(int n) {
+            array = new int[n];
+            this.size = n;
+        }
+
+        public static boolean isEmpty() {
+            return rear == -1;
+        }
+
+        // add / enqueue
+        public static void add(int data) {
+            if (rear == size - 1) {
+                System.out.println("Queue is full");
+                return;
+            }
+
+            rear++;
+            array[rear] = data;
+        }
+
+        // remove / dequeue
+        public static int remove() {
+            if (isEmpty()) {
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            int front = array[0];
+            for (int i = 0; i < rear; i++) {
+                array[i] = array[i + 1];
+            }
+            rear--;
+            return front;
+        }
+
+        // peek
+        public static int peek() {
+            if (isEmpty()) {
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            return array[0];
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue q = new Queue(4);
+        
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        while (!q.isEmpty()) {
+            System.err.println(q.peek());
+            q.remove();
+        }
+    }
+}
